e33b602ddc03472cca2bbc8edfc336951e68e300
Sapferum <stoeglazov90@gmail.com>
2025-01-20 22:21:55 +0100

    no changes

--- pages/descriptionAsceze.vue
+++ pages/descriptionAsceze.vue
@@ -12,7 +12,7 @@
 
 <style>
 .description {
-    background-image: url("../assets/images/panda-description.webp");
+    background-image: url("../assets/images/description_them.webp");
     width: 100%;
     height: 100vh;
     background-size: cover;
